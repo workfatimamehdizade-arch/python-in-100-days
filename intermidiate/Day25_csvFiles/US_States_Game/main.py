0f585941c4193d991f4f651fa63601b90403e04d
fatima <work.fatima.mehdizade@gmail.com>
2026-05-25 15:12:35 +0200

    fixed day 25 code  with list comprehension

--- intermidiate/Day25_csvFiles/US_States_Game/main.py
+++ intermidiate/Day25_csvFiles/US_States_Game/main.py
@@ -14,10 +14,7 @@ guessed_states = []
 while len(guessed_states) < len(all_states):
     answer_state = screen.textinput(title=f"{len(guessed_states)}/ 50 Sates Correct", prompt="What is another state's name?").title()
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
